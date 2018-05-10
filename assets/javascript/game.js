0f570c30d44228a78ac7c2c16a201df825013df2
ninner00 <macauleymail@gmail.com>
2018-05-09 22:12:59 -0400

    Word Guess Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,34 @@
 // referance to DOM - $ = DOM elements
-var $newGameButton = document.getElementById('new-game-button');
+var newGameButton = document.getElementById('new-game-button');
 var $underScore = document.getElementById('under-score');
 var $guessedLetters = document.getElementById('guessed-letters');
 var $guessesLeft = document.getElementById('guesses-left');
 var $wins = document.getElementById('wins');
 var $losses = document.getElementById('losses');
-var $aww = document.getElementById("aww"); 
-var $cheer = document.getElementById("cheer");
+
+var aww = document.getElementById("aww"); 
+var cheer = document.getElementById("cheer");
 
 
 // Create vars (wordBank, wins, losses, random word, guesses left, game running, random word placeholder, guessed letter bank, incorrect letter bank)
-var wordBank = ['Lager','Pilsner','India Pale Ale','Stout','Bitter','Pale Ale','Wheat Beer','Saison', 'Porter', 'Bock', 'Irish Red', 'Brown', 'Scotch', 'Rye', 'Amber', 'Vienna Lager'];
+var wordBank = ['Lager','Pilsner','IPA','Stout','Bitter','Pale','Wheat','Saison', 'Porter', 'Bock', 'Red', 'Brown', 'Scotch', 'Rye', 'Amber', 'Vienna'];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var gameRunning = false;
+var startGame = false;
 var randomWord = '';
-var randomWordUnderScoreArr = [];  // Needed for a push = array
-var guessedLetters = [];  // Needed for a push = array
+var randomWordUnderScore = [];  // Needed for a push = array
+var guessedLetters = [];  // Needed for a push = arra
 var wrongLetters = [];  // Needed for a push = array
 
 //newGame function to reset all stats, pick new word and create placeholders
 function newGame() {
 	// Resets
-	gameRunning = true;
-	guessesLeft =8;
+	startGame = true;
+	guessesLeft =9;
 	guessedLetters = [];
 	wrongLetters = [];
-	randomWordUnderScoreArr = [];
+	randomWordUnderScore = [];
 
 	// Pick a random word
 	randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
@@ -35,24 +36,24 @@ function newGame() {
 	// Under Scores of new word
 	for (var i = 0; i < randomWord.length; i++) {
 		if (randomWord[i] === ' ') {
-			randomWordUnderScoreArr.push('  ');
+			randomWordUnderScore.push(' ');
 		} 
 		else {
-			randomWordUnderScoreArr.push(' _ ');
+			randomWordUnderScore.push(' _ ');
 		}
 	}
 
 	// Write new stuff to DOM
 	$guessesLeft.textContent = guessesLeft;
-	$underScore.textContent = randomWordUnderScoreArr.join('');
+	$underScore.textContent = randomWordUnderScore.join('');
 	$guessedLetters.textContent = wrongLetters;	
 }
 
 // letterGuess function, takes in the letter you pressed and sees if its in the selected word
 function letterGuess(letter) {
-	console.log(letter);
+	console.log(randomWordUnderScore);
 
-	if (gameRunning === true && guessedLetters.indexOf(letter) === -1) {
+	if (startGame === true && guessedLetters.indexOf(letter) === -1) {
 		//Game Logic
 		guessedLetters.push(letter);
 
@@ -62,16 +63,16 @@ function letterGuess(letter) {
 			if (
 				randomWord[i].toLowerCase() === letter.toLowerCase()) {
 				// if matched, swap out to correct letter
-				randomWordUnderScoreArr[i] = randomWord[i];
+				randomWordUnderScore[i] = randomWord[i];
 			}
 		}
 
-		$underScore.textContent = randomWordUnderScoreArr.join('');
+		$underScore.textContent = randomWordUnderScore.join('');
 		// Pass letter to incorrect function
 		checkIncorrect(letter);
 	}
 	else {
-		if (gameRunning === false) { 
+		if (startGame === false) { 
 			alert("You need to start the game, please click the button.");
 		}
 		else {
@@ -84,9 +85,9 @@ function letterGuess(letter) {
 function checkIncorrect(letter) {
 	// Check to see if it is the wrong letter
 	if (
-		randomWordUnderScoreArr.indexOf(letter.toLowerCase()) === -1 
+		randomWordUnderScore.indexOf(letter.toLowerCase()) === -1 
 		&& 
-		randomWordUnderScoreArr.indexOf(letter.toUpperCase()) === -1
+		randomWordUnderScore.indexOf(letter.toUpperCase()) === -1
 		) 
 		{
 		// Minus a guess
@@ -105,33 +106,34 @@ function checkIncorrect(letter) {
 function checkLoss() {
 	if (guessesLeft === 0) {
 		losses++;
-		gameRunning = false;
+		startGame = false;
 		$losses.textContent = losses;
 		$underScore.textContent = randomWord;
-		$aww.play();
+		aww.play();
 	}
 	checkWin();
 }
 
 // checkWin
 function checkWin() {
-	if (randomWord.toLowerCase() === randomWordUnderScoreArr.join('').toLowerCase())
+	if (randomWord.toLowerCase() === randomWordUnderScore.join('').toLowerCase())
 	{
 		wins++;
-		gameRunning = false;
+		startGame = false;
 		$wins.textContent = wins;
-		$cheer.play(); 
+		cheer.play(); 
 	}
 }
 
 // play audio for win or loss
 function playAudio() { 
-    $aww.play(); 
-    $cheer.play(); 
+    aww.play(); 
+    cheer.play(); 
 } 
 
 // Add event listener for new game button
-$newGameButton.addEventListener('click', newGame);
+newGameButton.addEventListener('click', newGame);
+
 
 // Add onkeyup event to trigger letterGuess 65 to 90 is A to Z
 document.onkeyup = function(event) {
